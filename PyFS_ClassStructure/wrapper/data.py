dce2b979eff5a934379c5090a0d5a40d67f15ad6
soumitri2001 <56516406+soumitri2001@users.noreply.github.com>
2021-08-01 15:58:50 +0530

    attempted class structure

---
+++ PyFS_ClassStructure/wrapper/data.py
@@ -0,0 +1,24 @@
+class Solution():
+    #structure of the solution
+    def __init__(self):
+        self.num_features = None
+        self.num_agents = None
+        self.max_iter = None
+        self.obj_function = None
+        self.execution_time = None
+        self.convergence_curve = {}
+        self.best_agent = None
+        self.best_fitness = None
+        self.best_accuracy = None
+        self.final_population = None
+        self.final_fitness = None
+        self.final_accuracy = None
+
+
+class Data():
+    # structure of the training data
+    def __init__(self):
+        self.train_X = None
+        self.train_Y = None
+        self.val_X = None
+        self.val_Y = None
---
+++ PyFS_ClassStructure/wrapper/math_inspired/SCA.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+
+from PyFS_ClassStructure.wrapper.algorithm import Algorithm
+
+class SCA(Algorithm):
+    def __init__(self):
+        pass
+
+
+    def optimize(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    data = datasets.load_digits()
